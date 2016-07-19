392b9893ae787231c72e78358441bad37241e567
Tomáš Čech <sleep_walker@gnu.org>
2016-07-19 15:03:03 +0200

    services: networking: export *-configuration and *-service-type.
    * gnu/services/networking.scm: export *-configuration
      and *-service-type.

--- gnu/services/networking.scm
+++ gnu/services/networking.scm
@@ -39,13 +39,21 @@
   #:use-module (srfi srfi-26)
   #:use-module (ice-9 match)
   #:export (%facebook-host-aliases
+            static-networking
             static-networking-service
+            static-networking-service-type
             dhcp-client-service
             %ntp-servers
+            ntp-configuration
             ntp-service
+            ntp-service-type
+            tor-configuration
             tor-hidden-service
             tor-service
+            tor-service-type
+            bitlbee-configuration
             bitlbee-service
+            bitlbee-service-type
             wicd-service
             network-manager-service
             connman-service))
